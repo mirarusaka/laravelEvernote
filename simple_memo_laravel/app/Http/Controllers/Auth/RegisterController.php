523a2302c9c31474e146ee5d5f1cf801465637cb
69safe <endokun32@yahoo.co.jp>
2021-10-31 10:03:12 +0900

    ログイン画面実装

--- simple_memo_laravel/app/Http/Controllers/Auth/RegisterController.php
+++ simple_memo_laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -46,11 +46,14 @@ class RegisterController extends Controller
 
     public function register(UserCreateRequest $request)
     {
-        User::create([
-        'name' => $request->name,
-        'email' => $request->email,
-        'password' => Hash::make($request->password),
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
         ]);
+
+        $this->guard()->login($user);
+
         return redirect()->route('memo.index');
     }
 }
--- simple_memo_laravel/app/Http/Middleware/RedirectIfAuthenticated.php
+++ simple_memo_laravel/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,13 +17,16 @@ class RedirectIfAuthenticated
      * @param  string|null  ...$guards
      * @return mixed
      */
+
+     //$next($request) これがコントローラーアクション。returnに配置すればコントローラー前に、
+     //$responseをreturnに配置すればコントローラー後に処理できる。($nextはメソッドの一番初めに置く)
     public function handle(Request $request, Closure $next, ...$guards)
     {
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return redirect('/memo');
             }
         }
 
